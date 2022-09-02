bab0f2d7e83fe086ac4955697d5a66af17792359
RAVVA2799 <80970669+RAVVA2799@users.noreply.github.com>
2022-09-02 11:43:52 +0530

    Add files via upload

---
+++ esarrow.js
@@ -0,0 +1,14 @@
+
+//JavaScript Arrow Functions//
+
+//With arrow functions, you don't have to type the function keyword, the return keyword, and the curly brackets//
+
+//Arrow functions are not supported in IE11 or earlier.</p>//
+
+//<p id="demo"></p>//
+
+const x = (x, y) => x * y;
+console.log(x(5, 8));
+
+const a =(a,b)=>a/b;
+console.log(a(20,4));
